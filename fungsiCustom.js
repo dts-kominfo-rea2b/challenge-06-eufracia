1f7f44bc67d6e83fbd3376590d2a1b648dc96751
eufracia <ziporalicia@gmail.com>
2022-06-11 23:32:12 +0700

    Add bacaData function callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,38 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let arrData = [];
+
+  fs.readFile(file1, { encoding: "utf-8" }, (err, data) => {
+    if (err) fnCallback(err);
+
+    let theData = JSON.parse(data);
+    let theMessage = theData.message.split(" ");
+
+    arrData.push(theMessage[1]);
+
+    fs.readFile(file2, { encoding: "utf-8" }, (err, data) => {
+      if (err) fnCallback(err);
+
+      let theData = JSON.parse(data);
+      let theMessage = theData[0].message.split(" ");
+
+      arrData.push(theMessage[1]);
+
+      fs.readFile(file3, { encoding: "utf-8" }, (err, data) => {
+        if (err) fnCallback(err);
+
+        let theData = JSON.parse(data);
+        let theMessage = theData[0].data.message.split(" ");
+
+        arrData.push(theMessage[1]);
+
+        return fnCallback(err, arrData);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
